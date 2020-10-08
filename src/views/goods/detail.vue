830544b483fd4fe47d25eb41ab1e25779a76f2d1
朱言利 <3161656704@qq.com>
2020-10-08 17:36:25 +0800

    'second'

--- src/views/goods/detail.vue
+++ src/views/goods/detail.vue
@@ -16,6 +16,61 @@
       </div>
       <div class="buy">加入购物车</div>
     </div>
+    <div class="split"></div>
+    <div class="info">
+      <h1 class="title">商品信息</h1>
+      <p class="text">
+        一碗皮蛋瘦肉粥，总是我到粥店时的不二之选。香浓软滑，饱腹暖心，皮蛋的Q弹与瘦肉的滑嫩伴着粥香溢于满口，让人喝这样的一碗粥也觉得心满意足
+      </p>
+    </div>
+    <div class="split"></div>
+    <div class="rating">
+      <h1 class="title">商品评价</h1>
+      <div class="ratingselect">
+        <div class="rating-type border-1px">
+          <span class="block positive active">
+            全部
+            <span class="count">3</span>
+          </span>
+          <span class="block positive active">
+            推荐
+            <span class="count">2</span>
+          </span>
+          <span class="block positive active">
+            吐槽
+            <span class="count">1</span>
+          </span>
+        </div>
+        <!-- 开关 -->
+        <div class="switch">
+          <span class="icon-check-circle"></span>
+          <span class="text">只看有评价的内容</span>
+        </div>
+      </div>
+      <div class="rating-wrapper">
+        <ul>
+          <li>
+            <div class="user">
+              <span class="name">123456****</span>
+              <!-- <img
+                src="http://static.galileo.xiaojukeji.com/static/tms/default_header.png"
+                alt=""
+                height="12"
+                class="avatar"
+              /> -->
+            </div>
+            <div class="time">2020-10-08 10:30</div>
+            <p class="text">
+              <span></span>
+              <span>很喜欢的粥</span>
+            </p>
+          </li>
+          <li></li>
+          <li></li>
+        </ul>
+        <div class="no-rating" style="display:none">暂无评价</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -81,5 +136,116 @@ img {
   color: #fff;
   background: #00a0dc;
   opacity: 1;
+}
+.split {
+  width: 100%;
+  height: 16px;
+  border-top: 1px solid rgba(7, 17, 27, 0.1);
+  border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+  background: #f3f5f7;
+}
+.info {
+  padding: 18px;
+}
+.info .title {
+  line-height: 14px;
+  margin-bottom: 6px;
+  font-size: 14px;
+  color: #07111b;
+}
+.info .text {
+  line-height: 24px;
+  padding: 0 8px;
+  font-size: 12px;
+  color: #4d555d;
+}
+.rating {
+  padding-top: 18px;
+}
+.rating .title {
+  line-height: 14px;
+  margin-left: 18px;
+  font-size: 14px;
+  color: #07111b;
+}
+.ratingselect .rating-type {
+  padding: 18px 0;
+  margin: 0 18px;
+  position: relative;
+  font-size: 0;
+}
+.ratingselect .rating-type .block.positive.active {
+  background: #00a0dc;
+}
+.ratingselect .rating-type .block.positive {
+  background: rgba(0, 160, 220, 0.2);
+}
+.ratingselect .rating-type .block.active {
+  color: #fff;
+}
+.ratingselect .rating-type .block {
+  display: inline-block;
+  padding: 8px 12px;
+  margin-right: 8px;
+  line-height: 16px;
+  border-radius: 1px;
+  font-size: 12px;
+  color: #4d555d;
+}
+.ratingselect .rating-type .block .count {
+  margin-left: 2px;
+  font-size: 8px;
+}
+.ratingselect .rating-type .block.positive {
+  background: rgba(0, 160, 220, 0.2);
+}
+.ratingselect .rating-type .block {
+  display: inline-block;
+  padding: 8px 12px;
+  margin-right: 8px;
+  line-height: 16px;
+  border-radius: 1px;
+  font-size: 12px;
+  color: #4d555d;
+}
+.ratingselect .rating-type .block.negative {
+  background: rgba(77, 85, 93, 0.2);
+}
+.ratingselect .rating-type .block {
+  display: inline-block;
+  padding: 8px 12px;
+  margin-right: 8px;
+  line-height: 16px;
+  border-radius: 1px;
+  font-size: 12px;
+  color: #4d555d;
+}
+.ratingselect .switch {
+  padding: 12px 18px;
+  line-height: 24px;
+  border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+  color: #93999f;
+  font-size: 0;
+}
+.ratingselect .switch.on .icon-check_circle {
+  color: #00c850;
+}
+.ratingselect .switch .icon-check_circle {
+  display: inline-block;
+  margin-right: 4px;
+  font-size: 24px;
+}
+.ratingselect .switch .text {
+  display: inline-block;
+  vertical-align: top;
+  font-size: 12px;
+}
+.rating .rating-wrapper {
+  padding: 0 18px;
+}
+.rating .rating-wrapper .rating-item {
+  position: relative;
+  padding: 16px 0;
+  position: relative;
 }
 </style>
--- src/views/ratings/index.vue
+++ src/views/ratings/index.vue
@@ -1,38 +1,66 @@
 <template>
-  <div class="ratings">
+  <div class="rating">
     <Header></Header>
-    <div class="overview">
-      <div class="overview-left">
-        <h1 class="overview">4.2</h1>
-        <div class="title">综合评分</div>
-        <div class="rank">高于周边商家69.2%</div>
-      </div>
-      <div class="overview-right">
-        <div class="score-wrapper">
-          <span class="title">服务态度</span>
-          <div class="star">
-            <van-rate
-              v-model="value"
-              :size="15"
-              color="#ffd21e"
-              void-icon="star"
-              void-color="#eee"
-            />
+    <div class="ratings">
+      <div class="overview">
+        <div class="overview-left">
+          <h1 class="score">4.2</h1>
+          <div class="title">综合评分</div>
+          <div class="rank">高于周边商家69.2%</div>
+        </div>
+        <div class="overview-right">
+          <div class="score-wrapper">
+            <span class="title">服务态度</span>
+            <div class="star">
+              <van-rate
+                v-model="value"
+                :size="15"
+                color="#ffd21e"
+                void-icon="star"
+                void-color="#eee"
+              />
+            </div>
+          </div>
+          <div class="score-wrapper">
+            <span class="title">商品评分</span>
+            <div class="star">
+              <van-rate
+                v-model="value"
+                :size="15"
+                color="#ffd21e"
+                void-icon="star"
+                void-color="#eee"
+              />
+            </div>
           </div>
         </div>
-        <div class="score-wrapper">
-          <span class="title">商品评分</span>
-          <div class="star">
-            <van-rate
-              v-model="value"
-              :size="15"
-              color="#ffd21e"
-              void-icon="star"
-              void-color="#eee"
-            />
-          </div>
-        </div>
       </div>
+      <div class="split"></div>
+      <div class="ratingselect">
+        <div class="rating-type">
+          <span class="block positive">
+            全部
+            <span class="count">24</span>
+          </span>
+          <span class="block positive">
+            满意
+            <span class="count">18</span>
+          </span>
+          <span class="block negative">
+            不满意
+            <span class="count">6</span>
+          </span>
+        </div>
+        <div class="switch"></div>
+      </div>
+      <!-- <div class="ratingselect">
+        <van-tabs v-model="active">
+          <van-tab v-for="item in index" :key="item">
+            <template #title>{{ item }} </template>
+            内容 {{ item }}
+          </van-tab>
+        </van-tabs>
+      </div> -->
     </div>
   </div>
 </template>
@@ -44,6 +72,8 @@ export default {
   data() {
     return {
       value: 2.5,
+      active: 0,
+      index: ["全部", "满意", "不满意"],
     };
   },
   computed: {},
@@ -55,4 +85,107 @@ export default {
   methods: {},
 };
 </script>
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+.ratings {
+  position: absolute;
+  top: 174px;
+  bottom: 0;
+  left: 0;
+  width: 100%;
+  overflow: hidden;
+}
+.ratings .overview {
+  display: -webkit-box;
+  display: -ms-flexbox;
+  display: flex;
+  padding: 18px 0;
+}
+.ratings .overview .overview-left {
+  -webkit-box-flex: 0;
+  -ms-flex: 0 0 137px;
+  flex: 0 0 137px;
+  padding: 6px 0;
+  width: 137px;
+  border-right: 1px solid rgba(7, 17, 27, 0.1);
+  text-align: center;
+}
+.ratings .overview .overview-left .score {
+  margin-bottom: 6px;
+  line-height: 28px;
+  font-size: 24px;
+  color: #f90;
+}
+.ratings .overview .overview-left .title {
+  margin-bottom: 8px;
+  line-height: 12px;
+  font-size: 12px;
+  color: #07111b;
+}
+.ratings .overview .overview-left .rank {
+  line-height: 10px;
+  font-size: 10px;
+  color: #93999f;
+}
+.ratings .overview .overview-right {
+  -webkit-box-flex: 1;
+  -ms-flex: 1;
+  flex: 1;
+  padding: 6px 0 6px 24px;
+}
+.split {
+  width: 100%;
+  height: 16px;
+  border-top: 1px solid rgba(7, 17, 27, 0.1);
+  border-bottom: 1px solid rgba(7, 17, 27, 0.1);
+  background: #f3f5f7;
+}
+.ratingselect .rating-type {
+  padding: 18px 0;
+  margin: 0 18px;
+  position: relative;
+  font-size: 0;
+}
+.block.positive {
+  background: rgba(0, 160, 220, 0.2);
+}
+.ratingselect .rating-type .block {
+  display: inline-block;
+  padding: 8px 12px;
+  margin-right: 8px;
+  line-height: 16px;
+  border-radius: 1px;
+  font-size: 12px;
+  color: #4d555d;
+}
+.ratingselect .rating-type .block .count {
+  margin-left: 2px;
+  font-size: 8px;
+}
+.ratingselect .rating-type .block.positive.active {
+  background: #00a0dc;
+}
+.ratingselect .rating-type .block.active {
+  color: #fff;
+}
+.ratingselect .rating-type .block {
+  display: inline-block;
+  padding: 8px 12px;
+  margin-right: 8px;
+  line-height: 16px;
+  border-radius: 1px;
+  font-size: 12px;
+  color: #4d555d;
+}
+.ratingselect .rating-type .block.negative {
+  background: rgba(77, 85, 93, 0.2);
+}
+.ratingselect .rating-type .block {
+  display: inline-block;
+  padding: 8px 12px;
+  margin-right: 8px;
+  line-height: 16px;
+  border-radius: 1px;
+  font-size: 12px;
+  color: #4d555d;
+}
+</style>
